e85bc7b9055ad4fb4e5ff6b239b502fc412276db
vchavkov <vchavkov@minervanetworks.com>
2022-02-25 17:20:33 +0200

    Update: run pamaters

--- dev.sh
+++ dev.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 source .config.sh
 
-rm -rf public/*
+## clean
+rm -rf dist/*
 
-docker run --rm -it \
-  -v $(pwd):/src \
-  -p 1313:1313 \
-  ${DOCKER_IMAGE_TAG} \
-  server \
-  --bind=${BIND_IP_ADDRESS} \
-  --baseURL=${FQDN}
-
+CMD="docker run -it \
+--rm \
+-v $(pwd):/src \
+-v $(pwd)/dist:/dist \
+-p 1313:1313 \
+${IMAGE_TAG} \
+server \
+--bind=${BIND_IP_ADDRESS} \
+--baseURL=${FQDN}"
+printf "\n$CMD\n"
+eval "$CMD"
